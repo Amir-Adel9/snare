8e373a00410be0058c1548411fff1f1a19071db6
Amir-Adel9 <amiradel7152@gmail.com>
2025-01-06 00:50:27 +0200

    Nativewind and Gluestack setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,41 @@
-import { Stack } from "expo-router";
+import '../global.css';
+import '../global.css';
+import '../global.css';
+
+import { Stack } from 'expo-router';
+
+import { PortalHost } from '@rn-primitives/portal';
+import { GluestackUIProvider } from '~/components/ui/gluestack-ui-provider';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GluestackUIProvider mode='dark'>
+      <Stack
+        screenOptions={{
+          statusBarBackgroundColor: '#111111',
+          headerStyle: {
+            backgroundColor: '#111111',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen
+          name='index'
+          options={{
+            title: 'Home',
+          }}
+        />
+        <Stack.Screen
+          name='details'
+          options={{
+            title: 'Details',
+          }}
+        />
+        <PortalHost />
+      </Stack>
+    </GluestackUIProvider>
+  );
 }
